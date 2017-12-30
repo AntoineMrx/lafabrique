9133c039f1b06cd09531880e25824fb6248b3be0
Antoine Marmoux <antoine.marmoux@hotmail.fr>
2017-12-30 14:00:09 +0100

    adding models and db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171229183830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string "name"
+    t.string "characteristic"
+    t.string "description"
+    t.float "priceHalf"
+    t.float "pricePint"
+    t.float "pricePitcher"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string "name"
+    t.string "characteristic"
+    t.string "description"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "date"
+    t.float "price"
+    t.string "characteristic"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
