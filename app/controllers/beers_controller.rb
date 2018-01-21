9bfab2affacc7e0653f39acb03a6a7a38cee1c94
Antoine Marmoux <antoine.marmoux@hotmail.fr>
2018-01-21 18:20:51 +0100

    adding beers index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,7 @@
 class BeersController < ApplicationController
-
+before_action :set_beer, only: [:show, :edit, :update, :destroy]
   def index
+    @beers = Beer.all
   end
 
   def show
@@ -18,4 +19,71 @@ class BeersController < ApplicationController
   def create
   end
 
+  private
+
+  def set_beer
+    @beer = beer.find(params[:id])
+  end
+
+  def beer_params
+     params.require(:beer).permit(:name, :description, :characteristic, :priceHalf, :pricePint, :pricePitcher)
+  end
+
 end
+
+# class ProductsController < ApplicationController
+#   before_action :set_product, only: [:show, :edit, :update, :destroy]
+#   def index
+#     @products = Product.all
+#   end
+
+#   def search
+#     @word = params[:word]
+#     @productList = []
+#     Product.all.each do |product|
+#       @productList << product if product.name.downcase.include?(@word.downcase)
+#     end
+#     p @productList
+
+#   end
+
+#   def show
+#     @seller = @product.seller
+#   end
+
+#   def new
+#     @product = Product.new
+#     @seller = Seller.find(params[:seller_id])
+#   end
+
+#   def edit
+#   end
+
+#   def update
+#     @product.update(product_params)
+#     unless request.referrer == seller_url
+#       redirect_to @product
+#     end
+#   end
+
+#   def create
+#     @product = Product.new(product_params)
+#     @product.seller = Seller.find(params[:seller_id])
+#     @product.save
+#     redirect_to @product
+#   end
+
+#   def destroy
+#     @product.destroy
+#   end
+
+#   private
+
+#   def set_product
+#     @product = Product.find(params[:id])
+#   end
+
+#   def product_params
+#      params.require(:product).permit(:name, :description, :stock, :material, :color, :size, :weight, :selling_price, :retail_price, :vat, :seller_id, :category_id, :photo_1, :photo_1_cache, :photo_2, :photo_2_cache, :photo_3, :photo_3_cache)
+#   end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,14 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create(  name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Starting seeding"
+Beer.create( name: 'Blonde',  description: 'Bière Blonde',  characteristic: 'Bière Blonde légère',  priceHalf: 3.20,  pricePint: 6.40,  pricePitcher: 12)
+p "one"
+Beer.create( name: 'Rousse',  description: 'Bière Rousse',  characteristic: 'Bière Rousse légère',  priceHalf: 3.20,  pricePint: 6.40,  pricePitcher: 12)
+p "two"
+Beer.create( name: 'Blanche',  description: 'Bière Blanche',  characteristic: 'Bière Blanche légère',  priceHalf: 3.20,  pricePint: 6.40,  pricePitcher: 12)
+p "three, finish"
+
